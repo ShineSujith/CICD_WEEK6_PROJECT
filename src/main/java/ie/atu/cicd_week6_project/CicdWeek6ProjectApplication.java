442c2ad42a36c5d156d23effc6b5d1d6d748460f
Shine Sujith <g00414454@atu.ie>
2024-10-28 19:14:51 +0000

    Created Project

---
+++ src/main/java/ie/atu/cicd_week6_project/CicdWeek6ProjectApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd_week6_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdWeek6ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdWeek6ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/cicd_week6_project/CicdWeek6ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd_week6_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdWeek6ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
